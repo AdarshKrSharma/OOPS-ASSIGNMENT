817280421534a74e7d23a7cd24b4919fe8354527
AdarshKrSharma <adarshkumarsharma9234@gmail.com>
2026-01-09 12:10:56 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << "a = " << a << "\n";
+    cout << "b = " << b << "\n";
+    // storing original value of a in temporary variable c
+    c = a;
+    // swapping value of b in a
+    a = b;
+    // putting temporary variable i.e c's stored value in b
+    b = c;
+    cout << "Afer Swapping" << "\n";
+    cout << "a = " << a << "\n";
+    cout << "b = " << b;
+    return 0;
+}
---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double PI = 3.14; // PI cannot be changed
+
+int main()
+{
+    int radius;
+    cout << "Enter circle radius: ";
+    cin >> radius;
+    double area = PI * radius * radius;
+    cout << "Area of circle: " << area << endl;
+    return 0;
+}
